09102a1e07198a985db64da9bc47b5bfe00dacfc
Denis <den.maznev@gmail.com>
2024-07-19 02:06:06 +0300

    Change menu cell paradigm backdoor

--- frontend/src/components/main/Ship.jsx
+++ frontend/src/components/main/Ship.jsx
@@ -1,17 +1,50 @@
 import s from "./main.module.css"
 
-export function Ship({ id = null, shipNumber, isOnField, size = null, startX = null, startY = null, rotation = null }) {
+export function Ship({ id, shipNumber, isOnField, size, startX, startY, rotation, shipsLayout, refreshOccupiedCells}) {
 
     const dragStartHandler = (ev) => {
         const rect = ev.target.getBoundingClientRect();
         const offsetX = ev.clientX - rect.left;
         const offsetY = ev.clientY - rect.top;
         ev.dataTransfer.setData('text/plain', [offsetX, offsetY, shipNumber])
+        if (isOnField){
+            let newShipsLayout = shipsLayout.filter(ship => ship.shipNumber !== shipNumber);
+            refreshOccupiedCells(newShipsLayout);
+            //deOccupyCells(size, startX, startY, rotation)
+        }
     }
 
     const dragEndHandler = () => {
-        
+        refreshOccupiedCells(shipsLayout);
     }
+
+    // const deOccupyCells = (size, startX, startY, rotation) => {
+    //     const newCells = cells;
+    //     if (rotation === 'ver') {
+    //         for (let i = 0; i < size; i++) {
+    //             newCells[10 * (startY + i) + startX] = 0;
+    //         }
+    //     }
+    //     else {
+    //         for (let i = 0; i < size; i++) {
+    //             newCells[10 * startY + startX + i] = 0;
+    //         }
+    //     }
+    //     refreshBufferCells(newCells);
+    // }
+
+    // const refreshBufferCells = (newCells) => {
+    //     newCells = newCells.map((e) => e === 4 ? 0 : e);
+    //     for (let i in newCells) {
+    //         if (newCells[i] === 1) {
+    //             if (newCells?.[i - 10] === 0) newCells[i - 10] = 4;
+    //             if (newCells?.[parseInt(i) + 10] === 0) newCells[parseInt(i) + 10] = 4;
+    //             if (Math.floor(i / 10) !== 9 && newCells?.[parseInt(i) + 1] === 0) newCells[parseInt(i) + 1] = 4;
+    //             if (Math.floor(i / 10) !== 0 && newCells?.[i - 1] === 0) newCells[i - 1] = 4;
+    //         }
+    //     }
+    //     setCells(newCells);
+    // }
 
     if (!isOnField) {
         return (
@@ -20,6 +53,7 @@ export function Ship({ id = null, shipNumber, isOnField, size = null, startX = n
                 className={s.ship}
                 id={id}
                 onDragStart={(ev) => dragStartHandler(ev)}
+                onDragEnd={(ev) => dragEndHandler(ev)}
             >
             </div>
         );
@@ -31,6 +65,7 @@ export function Ship({ id = null, shipNumber, isOnField, size = null, startX = n
                 className={s.ship}
                 style={{position: "absolute", top: startY*45, left: startX*45, height: size*45-10, width: 35}}
                 onDragStart={(ev) => dragStartHandler(ev)}
+                onDragEnd={(ev) => dragEndHandler(ev)}
             >
             </div>
         );
@@ -42,6 +77,7 @@ export function Ship({ id = null, shipNumber, isOnField, size = null, startX = n
                 className={s.ship}
                 style={{position: "absolute", top: startY*45, left: startX*45, height: 35, width: size*45-10}}
                 onDragStart={(ev) => dragStartHandler(ev)}
+                onDragEnd={(ev) => dragEndHandler(ev)}
             >
             </div>
         );
--- frontend/src/components/main/game-menu.jsx
+++ frontend/src/components/main/game-menu.jsx
@@ -7,15 +7,46 @@ import { SetupField } from "./SetupField";
 
 export function GameMenu({ cells, setCells, insertIntoQueue }) {
     let [shipsLayout, setShipsLayout] = useState(Array(10).fill(null).map((e, index) => ({ shipNumber: index + 1, isOnField: false })))
+
+    const refreshOccupiedCells = (newShipsLayout) => {
+        const newCells = new Array(100).fill(0);
+        for (let ship of newShipsLayout.filter(ship => ship.isOnField)){
+            if (ship.rotation === 'ver'){
+                for (let i = 0; i < ship.size; i++) {
+                    newCells[10 * (ship.startY + i) + ship.startX] = 1;
+                }
+            }
+            else {
+                for (let i = 0; i < ship.size; i++) {
+                    newCells[10 * ship.startY + ship.startX + i] = 1;
+                }
+            }
+        }
+        refreshBufferCells(newCells);
+    }
+
+    const refreshBufferCells = (newCells) => {
+        newCells = newCells.map((e) => e === 4 ? 0 : e);
+        for (let i in newCells) {
+            if (newCells[i] === 1) {
+                if (newCells?.[i - 10] === 0) newCells[i - 10] = 4;
+                if (newCells?.[parseInt(i) + 10] === 0) newCells[parseInt(i) + 10] = 4;
+                if (i % 10 !== 9 && newCells?.[parseInt(i) + 1] === 0) newCells[parseInt(i) + 1] = 4;
+                if (i % 10 !== 0 && newCells?.[i - 1] === 0) newCells[i - 1] = 4;
+            }
+        }
+        setCells(newCells);
+    }
+
     return (
         <div className={mainStyles.gameMenu}>
-            <SetupField cells={cells} setCells={setCells} shipsLayout={shipsLayout} setShipsLayout={setShipsLayout}></SetupField>
-            <GameSetupPanel cells={cells} setCells={setCells} insertIntoQueue={insertIntoQueue} shipsLayout={shipsLayout} setShipsLayout={setShipsLayout} />
+            <SetupField cells={cells} refreshOccupiedCells={refreshOccupiedCells} shipsLayout={shipsLayout} setShipsLayout={setShipsLayout}></SetupField>
+            <GameSetupPanel refreshOccupiedCells={refreshOccupiedCells} insertIntoQueue={insertIntoQueue} shipsLayout={shipsLayout} setShipsLayout={setShipsLayout} />
         </div>
     );
 }
 
-function GameSetupPanel({ cells, setCells, insertIntoQueue, shipsLayout, setShipsLayout }) {
+function GameSetupPanel({ refreshOccupiedCells, insertIntoQueue, shipsLayout, setShipsLayout }) {
 
     const dragOverHandler = (ev) => {
         ev.preventDefault();
@@ -24,34 +55,48 @@ function GameSetupPanel({ cells, setCells, insertIntoQueue, shipsLayout, setShip
     const dropHandler = (ev) => {
         ev.preventDefault();
         const shipNumber = ev.dataTransfer.getData('text/plain').split(',')[2];
-        const shipRemoved = shipsLayout.filter(e => e.shipNumber===parseInt(shipNumber))[0];
-        if (shipRemoved.isOnField){
-            deOccupyCells(shipRemoved.size, shipRemoved.startX, shipRemoved.startY, shipRemoved.rotation);
-            let newShipsLayout = shipsLayout.filter(e => e.shipNumber!==parseInt(shipNumber));
+        const shipRemoved = shipsLayout.filter(e => e.shipNumber === parseInt(shipNumber))[0];
+        if (shipRemoved.isOnField) {
+            //deOccupyCells(shipRemoved.size, shipRemoved.startX, shipRemoved.startY, shipRemoved.rotation);
+            let newShipsLayout = shipsLayout.filter(e => e.shipNumber !== parseInt(shipNumber));
             const newShip = {
                 shipNumber: parseInt(shipNumber),
                 isOnField: false,
                 id: mainStyles['ship' + (shipNumber)]
             }
             newShipsLayout = [...newShipsLayout, newShip];
+            refreshOccupiedCells(newShipsLayout);
             setShipsLayout(newShipsLayout);
         }
     }
 
-    const deOccupyCells = (size, startX, startY, rotation) => {
-        const newCells = cells;
-        if (rotation === 'ver'){
-            for (let i = 0; i < size; i++){
-                newCells[10*(startY+i) + startX] = 0;
-            }
-        }
-        else {
-            for (let i = 0; i < size; i++){
-                newCells[10*startY + startX+i] = 0; 
-            }
-        }
-        setCells(newCells);
-    }
+    // const deOccupyCells = (size, startX, startY, rotation) => {
+    //     const newCells = cells;
+    //     if (rotation === 'ver') {
+    //         for (let i = 0; i < size; i++) {
+    //             newCells[10 * (startY + i) + startX] = 0;
+    //         }
+    //     }
+    //     else {
+    //         for (let i = 0; i < size; i++) {
+    //             newCells[10 * startY + startX + i] = 0;
+    //         }
+    //     }
+    //     refreshBufferCells(newCells);
+    // }
+
+    // const refreshBufferCells = (newCells) => {
+    //     newCells = newCells.map((e) => e === 4 ? 0 : e);
+    //     for (let i in newCells) {
+    //         if (newCells[i] === 1) {
+    //             if (newCells?.[i - 10] === 0) newCells[i - 10] = 4;
+    //             if (newCells?.[parseInt(i) + 10] === 0) newCells[parseInt(i) + 10] = 4;
+    //             if (Math.floor(i / 10) !== 9 && newCells?.[parseInt(i) + 1] === 0) newCells[parseInt(i) + 1] = 4;
+    //             if (Math.floor(i / 10) !== 0 && newCells?.[i - 1] === 0) newCells[i - 1] = 4;
+    //         }
+    //     }
+    //     setCells(newCells);
+    // }
 
     return (
         <div
@@ -62,7 +107,12 @@ function GameSetupPanel({ cells, setCells, insertIntoQueue, shipsLayout, setShip
             <div className={mainStyles.setupPanelHeading}>Расставьте свои судна</div>
             <div className={mainStyles.shipsContainer}>
                 {shipsLayout.filter(e => e.isOnField === false).map((e) => (
-                    <Ship key={e.shipNumber} shipNumber={e.shipNumber} isOnField={e.isOnField} id={mainStyles['ship' + (e.shipNumber)]}></Ship>
+                    <Ship
+                        key={e.shipNumber}
+                        shipNumber={e.shipNumber}
+                        isOnField={e.isOnField}
+                        id={mainStyles['ship' + (e.shipNumber)]}
+                    />
                 ))}
             </div>
             <Link to='/game'>
