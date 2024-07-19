a7b670de7cc23cb3c2127234f726ab007b593b52
Denis <den.maznev@gmail.com>
2024-07-19 03:19:13 +0300

    deleted comments

--- frontend/src/components/main/SetupField.jsx
+++ frontend/src/components/main/SetupField.jsx
@@ -43,7 +43,6 @@ export function SetupField({ cells, refreshOccupiedCells, shipsLayout, setShipsL
     }
 
     const shipMoveHandler = (shipNumber, size, start, rotation) => {
-        //let shipMoved = shipsLayout.filter(e => e.shipNumber === parseInt(shipNumber))[0];
         let newShipsLayout = shipsLayout.filter(e => e.shipNumber !== parseInt(shipNumber));
         const newShip = {
             shipNumber: parseInt(shipNumber),
@@ -55,10 +54,6 @@ export function SetupField({ cells, refreshOccupiedCells, shipsLayout, setShipsL
         }
         newShipsLayout = [...newShipsLayout, newShip];
         setShipsLayout(newShipsLayout);
-        // if (shipMoved.isOnField) {
-        //     deOccupyCells(shipMoved.size, shipMoved.startX, shipMoved.startY, shipMoved.rotation);
-        // }
-        //occupyCells(size, start, rotation);
         refreshOccupiedCells(newShipsLayout);
     }
 
@@ -76,66 +71,6 @@ export function SetupField({ cells, refreshOccupiedCells, shipsLayout, setShipsL
             return true;
         }
     }
-
-    // const refreshOccupiedCells = (newShipsLayout) => {
-    //     const newCells = new Array(100).fill(0);
-    //     for (let ship of newShipsLayout.filter(ship => ship.isOnField)){
-    //         if (ship.rotation === 'ver'){
-    //             for (let i = 0; i < ship.size; i++) {
-    //                 newCells[10 * (ship.startY + i) + ship.startX] = 1;
-    //             }
-    //         }
-    //         else {
-    //             for (let i = 0; i < ship.size; i++) {
-    //                 newCells[10 * ship.startY + ship.startX + i] = 1;
-    //             }
-    //         }
-    //     }
-    //     refreshBufferCells(newCells);
-    // }
-
-    // const occupyCells = (size, start, rotation) => {
-    //     const newCells = cells;
-    //     if (rotation === 'ver') {
-    //         for (let i = 0; i < size; i++) {
-    //             newCells[10 * (start[1] + i) + start[0]] = 1;
-    //         }
-    //     }
-    //     else {
-    //         for (let i = 0; i < size; i++) {
-    //             newCells[10 * start[1] + start[0] + i] = 1;
-    //         }
-    //     }
-    //     refreshBufferCells(newCells);
-    // }
-
-    // const deOccupyCells = (size, startX, startY, rotation) => {
-    //     const newCells = cells;
-    //     if (rotation === 'ver') {
-    //         for (let i = 0; i < size; i++) {
-    //             newCells[10 * (startY + i) + startX] = 0;
-    //         }
-    //     }
-    //     else {
-    //         for (let i = 0; i < size; i++) {
-    //             newCells[10 * startY + startX + i] = 0;
-    //         }
-    //     }
-    //     refreshBufferCells(newCells);
-    // }
-
-    // const refreshBufferCells = (newCells) => {
-    //     newCells = newCells.map((e) => e === 4 ? 0 : e);
-    //     for (let i in newCells) {
-    //         if (newCells[i] === 1) {
-    //             if (newCells?.[i - 10] === 0) newCells[i - 10] = 4;
-    //             if (newCells?.[parseInt(i) + 10] === 0) newCells[parseInt(i) + 10] = 4;
-    //             if (i % 10 !== 9 && newCells?.[parseInt(i) + 1] === 0) newCells[parseInt(i) + 1] = 4;
-    //             if (i % 10 !== 0 && newCells?.[i - 1] === 0) newCells[i - 1] = 4;
-    //         }
-    //     }
-    //     setCells(newCells);
-    // }
 
 
     return (
