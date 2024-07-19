a15b4528f83172700aa1744a24dcef9e769a633f
Denis <den.maznev@gmail.com>
2024-07-20 00:48:34 +0300

    pre turnable commit

--- frontend/src/components/main/Ship.jsx
+++ frontend/src/components/main/Ship.jsx
@@ -1,4 +1,12 @@
 import s from "./main.module.css"
+import size1ship from "./svg/size1ship.svg"
+import size2ship from "./svg/size2ship.svg"
+import size3ship from "./svg/size3ship.svg"
+import size4ship from "./svg/size4ship.svg"
+import size1shipVert from "./svg/size1shipVert.svg"
+import size2shipVert from "./svg/size2shipVert.svg"
+import size3shipVert from "./svg/size3shipVert.svg"
+import size4shipVert from "./svg/size4shipVert.svg"
 
 export function Ship({ id, shipNumber, isOnField, size, startX, startY, rotation, shipsLayout, refreshOccupiedCells}) {
 
@@ -6,6 +14,11 @@ export function Ship({ id, shipNumber, isOnField, size, startX, startY, rotation
         const rect = ev.target.getBoundingClientRect();
         const offsetX = ev.clientX - rect.left;
         const offsetY = ev.clientY - rect.top;
+
+        const img = new Image();
+        img.src = getShipImage(shipNumber);
+        ev.dataTransfer.setDragImage(img, offsetX, offsetY);
+
         ev.dataTransfer.setData('text/plain', [offsetX, offsetY, shipNumber])
         if (isOnField){
             let newShipsLayout = shipsLayout.filter(ship => ship.shipNumber !== shipNumber);
@@ -17,40 +30,54 @@ export function Ship({ id, shipNumber, isOnField, size, startX, startY, rotation
         if (isOnField) refreshOccupiedCells(shipsLayout);
     }
 
+    const getShipImage = (shipNumber, isOnField, rotation = null) => {
+        if (isOnField && rotation ==='hor'){
+            if (shipNumber === 1) return size4ship;
+            else if (shipNumber <= 3) return size3ship;
+            else if (shipNumber <= 6) return size2ship;
+            else return size1ship;
+        }
+        else if (isOnField && rotation ==='ver'){
+            if (shipNumber === 1) return size4shipVert;
+            else if (shipNumber <= 3) return size3shipVert;
+            else if (shipNumber <= 6) return size2shipVert;
+            else return size1shipVert;
+        }
+        else{
+            if (shipNumber === 1) return size4ship;
+            else if (shipNumber <= 3) return size3ship;
+            else if (shipNumber <= 5) return size2ship;
+            else if (shipNumber === 6) return size2shipVert;
+            else return size1ship;
+        }
+    }
+
     if (!isOnField) {
         return (
-            <div
+            <img
+                src={getShipImage(shipNumber, isOnField)}
                 draggable={true}
                 className={s.ship}
                 id={id}
                 onDragStart={(ev) => dragStartHandler(ev)}
                 onDragEnd={(ev) => dragEndHandler(ev)}
+                alt="ship"
             >
-            </div>
+            </img>
         );
     }
-    else if (rotation === 'ver') {
+    else {
         return (
-            <div
+            <img
+                src={getShipImage(shipNumber, isOnField, rotation)}
                 draggable={true}
                 className={s.ship}
-                style={{position: "absolute", top: startY*45, left: startX*45, height: size*45-10, width: 35}}
+                style={{position: "absolute", top: startY*45, left: startX*45}}
                 onDragStart={(ev) => dragStartHandler(ev)}
                 onDragEnd={(ev) => dragEndHandler(ev)}
+                alt="ship"
             >
-            </div>
-        );
-    }
-    else if (rotation === 'hor') {
-        return (
-            <div
-                draggable={true}
-                className={s.ship}
-                style={{position: "absolute", top: startY*45, left: startX*45, height: 35, width: size*45-10}}
-                onDragStart={(ev) => dragStartHandler(ev)}
-                onDragEnd={(ev) => dragEndHandler(ev)}
-            >
-            </div>
+            </img>
         );
     }
 }
